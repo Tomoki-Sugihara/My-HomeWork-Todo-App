67026f73a3ab808d3af31bb6cb116c7f20a1b4a2
sugihara10969 <sugihara.10969@gmail.com>
2020-07-03 21:56:26 +0900

    Updates

---
+++ precache-manifest.10c9285c115f6a09ab8d942f2c8adb7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7beb5b4a112b56032e88c4b50417cbb",
+    "url": "/My-Homework-Todo-App/index.html"
+  },
+  {
+    "revision": "ea359a18839142d88fd3",
+    "url": "/My-Homework-Todo-App/static/js/2.3f10510e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/My-Homework-Todo-App/static/js/2.3f10510e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac32f2368ec55790e6a5",
+    "url": "/My-Homework-Todo-App/static/js/main.8caebb5e.chunk.js"
+  },
+  {
+    "revision": "8e043a9a389c6be206ea",
+    "url": "/My-Homework-Todo-App/static/js/runtime-main.68846d9a.js"
+  }
+]);
